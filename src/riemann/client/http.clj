f02308ba8af8e7bf455089791b533344e85b6d37
Kyle Kingsbury <aphyr@aphyr.com>
2014-05-22 15:18:11 -0700

    HTTP client: fill in local hostname

--- src/riemann/client/http.clj
+++ src/riemann/client/http.clj
@@ -1,10 +1,38 @@
 (ns riemann.client.http
   "Dirty hack because we can't use protobufs in hadoop; CDH version conflict :("
   (:require [org.httpkit.client :as http]
+            [clojure.java.shell :as sh]
             [cheshire.core :as json]
             [clojure.string :as str]
             clj-time.format
             clj-time.coerce))
+
+(def hostname-refresh-interval
+  "How often to allow shelling out to hostname (1), in seconds."
+  60)
+
+(defn get-hostname
+  "Fetches the hostname by shelling out to hostname (1), whenever the given age
+  is stale enough. If the given age is recent, as defined by
+  hostname-refresh-interval, returns age and val instead."
+  [[age val]]
+  (if (and val (<= (* 1000 hostname-refresh-interval)
+                   (- (System/currentTimeMillis) age)))
+    [age val]
+    [(System/currentTimeMillis)
+     (let [{:keys [exit out]} (sh/sh "hostname")]
+       (if (= exit 0)
+         (.trim out)))]))
+
+(let [cache (atom [nil nil])]
+  (defn localhost
+    "Returns the local host name."
+    []
+    (if (re-find #"^Windows" (System/getProperty "os.name"))
+      (or (System/getenv "COMPUTERNAME") "localhost")
+      (or (System/getenv "HOSTNAME")
+          (second (swap! cache get-hostname))
+          "localhost"))))
 
 (defn client
   "Constructs a new HTTP client."
@@ -12,12 +40,17 @@
   (assert (:host opts))
   {:uri (str "http://" (:host opts) ":" (or (:port opts) 5556) "/events")})
 
-(defn ensure-time
-  "Make sure events have a time."
+(defn defaults
+  "Fill in default time and host."
   [event]
-  (if (:time event)
-    event
-    (assoc event :time (long (/ (System/currentTimeMillis) 1000)))))
+  (let [e (transient event)
+        e (if (contains? event :time)
+            e
+            (assoc! e :time (long (/ (System/currentTimeMillis) 1000))))
+        e (if (contains? event :host)
+            e
+            (assoc! e :host (localhost)))]
+    (persistent! e)))
 
 (defn unix->iso8601
   "Transforms unix time to iso8601 string"
@@ -34,7 +67,7 @@
   "Send some events over a client, synchronously"
   [client events]
   (let [events (->> events
-                    (map ensure-time)
+                    (map defaults)
                     (map event->json))]
     @(http/put (:uri client)
                {:body (str/join "\n" (conj events "\n"))})))
