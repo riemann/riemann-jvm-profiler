3a678830ab59785ceaa9a10ed27a6acac3bd5143
James Turnbull <james@lovedthanlost.net>
2022-06-25 13:41:33 -0400

    Added GH Actions testing
    Signed-off-by: James Turnbull <james@lovedthanlost.net>

--- project.clj
+++ project.clj
@@ -10,5 +10,7 @@
                  [clj-radix "0.1.0"]
                  [http-kit "2.1.16"]
                  [clj-time "0.7.0"]]
+  :plugins [[test2junit "1.3.3"]]
+  :test2junit-output-dir "target/test2junit"
   :java-source-paths ["src/riemann"]
   :manifest {"premain-Class" "riemann.jvm_profiler.Agent"})
